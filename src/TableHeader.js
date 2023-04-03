9b368f1a91fd0e98f43d312231a4fdb97b47de6c
Yskak <shiyap.yskak@gmail.com>
2023-04-03 12:26:49 +1000

    fixed up the table header alignment

--- src/TableHeader.js
+++ src/TableHeader.js
@@ -4,8 +4,10 @@ import './TableHeader.css';
 const TableHeader = () => {
   return (
     <div className="table-header">
-      <div className="header-name">Name</div>
-      <div className="header-ticker">Ticker</div>
+      <div className="header-coin-ticker"> 
+        <div className="header-coin">Coin</div>
+        <div className="header-ticker">Ticker</div>
+      </div> {/* Add this line */}
       <div className="header-price">Price</div>
       <div className="header-volume">24h Volume</div>
       <div className="header-price-change">24h Price Change</div>
